5a36df6fb22ad462f75b2b88db42e42968665d8b
muhamadilhamh <ilham.habib190600@gmail.com>
2022-04-02 22:20:42 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+      <Header />
+    <router-view></router-view>
+
+    <!-- <v-footer>
+      <Footer />
+    </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+import Header from "./components/common/Header";
+
+export default {
+  name: "App",
+
+  components: {
+    Header,
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
