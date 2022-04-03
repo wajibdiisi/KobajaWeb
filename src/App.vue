e1adf28784c633e90c1ec3af29e3fd4d4f075752
GedeBagus <Bdarmagita@gmail.com>
2022-04-03 21:31:33 +0700

    Push Template footer

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,22 @@
     <v-main>
       <router-view />
     </v-main>
+    <!-- <v-footer> -->
+      <Footer />
+    <!-- </v-footer> -->
   </v-app>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default {
   name: "App",
 
   components: {
     Header,
+    Footer,
   },
   data: () => ({
     //
