3c624963472318110488a803a3dc128094523853
muhamadilhamh <ilham.habib190600@gmail.com>
2022-04-03 21:14:40 +0700

    produk tab solve

--- src/views/About.vue
+++ src/views/About.vue
@@ -20,19 +20,19 @@
     <v-container>
       <v-tabs
           v-model="tabs"
-          background-color="transparent"
-          grow
+           background-color="transparent "
+            dark
         >
-          <v-tab>
-           Product 1
+          <v-tab color>
+            Product 1
           </v-tab>
-          <v-tab>
-           Product 2
+           <v-tab>
+            Product 2
           </v-tab>
-          <v-tab>
-           Product 3
+            <v-tab>
+            Product 3
           </v-tab>
-          <v-tab>
+           <v-tab>
            All
           </v-tab>
         </v-tabs>
@@ -41,44 +41,23 @@
       <v-tab-item>
         <v-card flat>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+           <v-img src="@/assets/logo kobaja v.2 trans.png"></v-img>
           </v-card-text>
         </v-card>
       </v-tab-item>
       <v-tab-item>
         <v-card flat>
-          <v-card-title class="text-h5">
-            An awesome title
-          </v-card-title>
-          <v-card-text>
-            <p>
-              Duis lobortis massa imperdiet quam. Donec vitae orci sed dolor rutrum auctor. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Praesent congue erat at massa.
-            </p>
-
-            <p>
-              Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Etiam sit amet orci eget eros faucibus tincidunt. Donec sodales sagittis magna.
-            </p>
-
-            <p class="mb-0">
-              Ut leo. Suspendisse potenti. Duis vel nibh at velit scelerisque suscipit. Fusce pharetra convallis urna.
-            </p>
-          </v-card-text>
+          <v-img src="@/assets/logo kobaja v.2 trans.png"></v-img>
         </v-card>
       </v-tab-item>
       <v-tab-item>
         <v-card flat>
-          <v-card-title class="text-h5">
-            An even better title
-          </v-card-title>
-          <v-card-text>
-            <p>
-              Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Sed hendrerit. Maecenas malesuada. Vestibulum ullamcorper mauris at ligula. Proin faucibus arcu quis ante.
-            </p>
-
-            <p class="mb-0">
-              Etiam vitae tortor. Curabitur ullamcorper ultricies nisi. Sed magna purus, fermentum eu, tincidunt eu, varius ut, felis. Aliquam lobortis. Suspendisse potenti.
-            </p>
-          </v-card-text>
+         <v-img src="@/assets/logo kobaja v.2 trans.png"></v-img>
+        </v-card>
+      </v-tab-item>
+       <v-tab-item>
+        <v-card flat>
+          <v-img src="@/assets/logo kobaja v.2 trans.png"></v-img>
         </v-card>
       </v-tab-item>
     </v-tabs-items>
@@ -91,7 +70,7 @@ export default {
   name: "Home",
    data () {
       return {
-       
+        tabs: null,
       }
     },
 };
